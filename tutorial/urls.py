918313e3a6c1a2babde83997d5f33cc65059bb0a
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-27 17:52:32 -0300

    Relationships and Hyperlinked APIs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
