cb3f1561b76262aa391b81a9d0faf1395e1aa781
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-28 17:44:01 -0300

    ViewSets

--- snippets/views.py
+++ snippets/views.py
@@ -2,18 +2,25 @@
 from __future__ import unicode_literals
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework import mixins
+from rest_framework import generics
 
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework import renderers
 
 from rest_framework.decorators import action
+from rest_framework import viewsets
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
